9bba596529e1e00bbbcb6e921490b6d6cfe449c5
NSHC <curic4t@gmail.com>
2020-02-07 17:23:09 +0900

    20200207

--- CipherKeyPad/app/src/main/java/com/example/cipherkeypad/MainActivity.java
+++ CipherKeyPad/app/src/main/java/com/example/cipherkeypad/MainActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent keyboardintent = new Intent(getApplicationContext(), MainkeyBoard.class);
 
-        startService(keyboardintent);
+        editText = findViewById(R.id.editText);
+        MainkeyBoard mainkeyBoard = (MainkeyBoard)findViewById(R.id.keyboard);
+        editText.setShowSoftInputOnFocus(false);
+
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setTextIsSelectable(true);
+
+        InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+        mainkeyBoard.setInputConnection(ic);
+        //startService(keyboardintent);
     }
 }
